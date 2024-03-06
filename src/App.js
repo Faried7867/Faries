d0edd2f3ed29bdf6eb5619cfb4c2826b4f5b42c9
Faried7867 <ffariedullah301@gmail.com>
2024-03-06 12:59:59 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ return (
 
   initial={{width:"100vw",height:"100vh",}}
    animate={{height:"0px",}}
-  transition={{delay:1.5,duration:2,ease:"easeIn"}}
+  transition={{delay:1.5,duration:2.5,ease:"easeIn"}}
 >
 
 </motion.div>
